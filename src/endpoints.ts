03fadd907a8011684dfc919a85c88588740f20d1
marihachi <marihachi0620@gmail.com>
2021-05-15 13:09:06 +0900

    endpoint

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -5,14 +5,88 @@ type TODO = Record<string, any>;
 type ShowUserReq = { username: string; host?: string; } | { userId: ID; };
 
 export type Endpoints = {
+	// admin
+
+	// announcements
+
+	// antennas
+	'antennas/create': { req: TODO; res: TODO; };
+
+	// ap
+
+	// app
+
+	// auth
+
+	// blocking
+
+	// channnels
+
+	// charts
+
+	// clips
+
+	// drive
+
+	// federation
+
+	// following
+
+	// gallery
+
+	// games
+
+	// get-online-users-count
+
+	// hashtags
+
+	// i
 	'i': { req: TODO; res: User; };
+
+	// messaging
+
+	// meta
 	'meta': { req: { detail?: boolean; }; res: Instance; };
 
-	'users': { req: { limit?: number; offset?: number; sort?: UserSorting; origin?: OriginType; }; res: User[]; };
-	'users/show': { req: ShowUserReq; res: User; } | { req: { userIds: ID[]; }; res: User[]; };
+	// miauth
 
+	// mute
+
+	// my
+
+	// notes
 	'notes': { req: { limit?: number; sinceId?: ID; untilId?: ID; }; res: Note[]; };
 	'notes/create': { req: TODO; res: { createdNote: Note }; };
 	'notes/delete': { req: { noteId: ID; }; res: null; };
 	'notes/show': { req: { noteId: ID; }; res: Note; };
+
+	// notifications
+
+	// page-push
+
+	// pages
+
+	// ping
+
+	// pinned-users
+
+	// promo
+
+	// request-reset-password
+
+	// reset-password
+
+	// room
+
+	// stats
+
+	// server-info
+
+	// sw
+
+	// username
+
+	// users
+	'users': { req: { limit?: number; offset?: number; sort?: UserSorting; origin?: OriginType; }; res: User[]; };
+	'users/show': { req: ShowUserReq; res: User; } | { req: { userIds: ID[]; }; res: User[]; };
 };
